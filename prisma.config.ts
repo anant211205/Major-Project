404824529dba1c55a81b4fb4cbe5739debd8f7fc
anant211205 <anant211205@gmail.com>
2025-11-01 23:40:10 +0530

    authentication added using clerk , added middleware

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/mydb",
+  },
+});
